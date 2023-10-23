20de17111d89e3bf41aea2bdf082c57f0c580008
Guilherme Ribeiro <guilhermehcr97@gmail.com>
2023-10-22 22:51:58 -0300

    navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
-import "./globals.css";
 import Providers from "../providers/Providers";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "IMDB Clone",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
