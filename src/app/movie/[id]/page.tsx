af65c2371b00a09619d345fa69d17c67649d1a72
Guilherme Ribeiro <guilhermehcr97@gmail.com>
2023-11-07 00:09:56 -0300

    add movie page

---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,61 @@
+import { IMovie } from "@/types/MovieData";
+import Image from "next/image";
+import React from "react";
+
+const API_KEY = process.env.API_KEY;
+
+const getMovie = async (movieId: string) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`,
+    {
+      next: { revalidate: 10000 },
+    }
+  );
+
+  return await res.json();
+};
+
+export default async function MoviePage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const movieId = params?.id;
+  const movie: IMovie = await getMovie(movieId);
+
+  return (
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            movie?.backdrop_path || movie?.poster_path
+          }`}
+          alt="image is not available"
+          width={500}
+          height={300}
+          style={{ maxWidth: "100%", height: "100%" }}
+          className="rounded-lg"
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+        />
+        <div className="p-2">
+          <h2 className="text-lg mb-3 font-bold">{movie?.title}</h2>
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">Overview:</span>
+            {movie?.overview}
+          </p>
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">Date Released:</span>
+            {movie?.release_date}
+          </p>
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">Rating:</span>
+            {movie?.vote_count}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
